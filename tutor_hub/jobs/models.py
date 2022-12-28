9b3e1e3bf943bf9f3b2aadcfdf62e7e659f78c77
Anurag7985 <kanurag.ofc@gmail.com>
2022-12-28 16:22:25 +0530

    writing models.p y

--- tutor_hub/jobs/models.py
+++ tutor_hub/jobs/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    contact_number = models.CharField(max_length=15)
+
+class Tutor(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    contact_number = models.CharField(max_length=15)
+
+class Job(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    hourly_rate = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Application(models.Model):
+    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    cover_letter = models.TextField()
+
