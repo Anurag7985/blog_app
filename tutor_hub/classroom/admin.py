6d165d05f8de255252661bcbcd7031513407aff2
Anurag7985 <kanurag.ofc@gmail.com>
2023-01-30 13:15:43 +0530

    up

--- tutor_hub/classroom/admin.py
+++ tutor_hub/classroom/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TutorReg
+from .models import StudentReg
 
 # Register your models here.
+admin.site.register(TutorReg)
+admin.site.register(StudentReg)
