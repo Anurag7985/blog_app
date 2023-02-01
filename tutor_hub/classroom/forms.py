eaf47a538ea47e26d6216bbb5e2ce921f86b7a7c
Anurag7985 <kanurag.ofc@gmail.com>
2023-02-01 22:13:41 +0530

    upd

--- tutor_hub/classroom/forms.py
+++ tutor_hub/classroom/forms.py
@@ -1,10 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
-from django import forms
-
 
 class SignUpForm(UserCreationForm):
     class Meta:
-        model = (UserCreationForm)
+        model = UserCreationForm
         fields = ['username', 'first_name', 'last_name', 'email']
 # Change the default email name from Email address to Email
         labels = {'email': 'Email'}
