62dc3df3d92270b4d32cb56d45f941bc65db65cf
Anurag7985 <kanurag.ofc@gmail.com>
2023-02-08 23:17:52 +0530

    up

--- tutor_hub/classroom/models.py
+++ tutor_hub/classroom/models.py
@@ -3,14 +3,12 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your models here.
 
 class TutorReg(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='tutor')
     name = models.CharField(max_length=255, default='default name')
     email = models.EmailField(max_length=255, default='default email')
     subject_experties = models.CharField(max_length=100, default='default subject')
     bio = models.TextField(default='')
 
 class StudentReg(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,  related_name='student')
     name = models.CharField(max_length=100, default='default name')
     email = models.EmailField(default='default email')
     subject_required = models.CharField(max_length=100, default='default subject')
